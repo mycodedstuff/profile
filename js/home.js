4444488dbd75be95757e9fd3cd3b03dca94c11fe
Aman Singh <singh.aman956@gmail.com>
2017-07-16 18:26:44 +0530

    Delete home.js

--- js/home.js
+++
@@ -1,7 +0,0 @@
-function hideLoading(){
-    setTimeout(function(){
-        $(".back").slideUp("slow");
-        $("body").css("overflow-y","auto");
-        $("#mainWebpage").fadeIn("slow","linear");
-    },2000);
-}
